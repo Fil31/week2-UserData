6d6412ec95b7de3a9f2fea3088d996a2cd3c0dc9
Fil31 <lolid76@gmail.com>
2022-03-18 10:47:43 +0300

    ne znayu 4to delayu

---
+++ UserData/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Спрашивает у юзера его имя и пароль
+            // Если имя совпадает с зарегистрированным юзером admin, то консоль приветствует юзера Welcome, user
+            // В ином случае ошибка Sorry, but username not registered.
+            Console.WriteLine("Hey! Please enter your username");
+            string username = Console.ReadLine();
+            if(username == "admin")
+            {
+                Console.WriteLine($"Welcome, {username}!");
+            }
+            else 
+            {
+                Console.WriteLine($"Sorry, but username {username} not registered.");
+            }
+            if (username == "admin")
+            {
+                Console.WriteLine("Have a good day!");
+            }
+            else 
+            {
+                Console.WriteLine("You can register at https://www.cisco.com/");
+            }
+           // Я знаю что это можно было уместить в гораздо меньше строк, но для практики я считаю можно сделать так.
+        }
+    }
+}
---
+++ UserPIN/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UserPIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для входа в Swedbank требуется ввести ваш PIN");
+            string pin = Console.ReadLine();
+            if(pin == "1234")
+                Console.WriteLine("Добро пожаловать в Swedbank!");
+            else
+            {
+                Console.WriteLine("Введеным Вами PIN неверный");
+            }
+            if (pin == "1234")
+            {
+                Console.WriteLine("Напомните, как Вас зовут?");
+                string name = Console.ReadLine();
+                Console.WriteLine($"Рады вновь видеть Вас в банке, который забыл как вас зовут, {name}!");
+            }
+
+            //Приложение просит юзера ввести пинкод, если введеный PIN "1234", то приложение приветсвует юзера
+            //*и спрашивает его имя, а затем приветствует по имени его.
+            //В ином случае, консоль выдаст сообщение об ошибке Invalid PIN. 
+            
+        }
+    }
+}
