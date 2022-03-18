35cd3d3efe52dd5a08c55fad39dac5a9da02386a
Fil31 <lolid76@gmail.com>
2022-03-18 11:22:19 +0300

    4 zadaniya

---
+++ DrivingLicense/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DrivingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter your year of birth");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+            int result = 2022 - yearOfBirth;
+                if (result < 18)
+            {
+                Console.WriteLine("You are too young for driving license.");
+            }
+            else if (result > 18)
+            {
+                Console.WriteLine("Drive carefully");
+            }
+                else
+                    {
+                Console.WriteLine("Congratulations, you may apply for your driving license now");
+            }
+            
+
+
+            //Приложение просит юзера ввести его год рождения, 
+            //Приложение вычисляет возраст юзера (2022 - текущий год юзера)
+            //Если юзер младше 18 лет, то в консоли будет "Too young for driving license"
+            //Если старше 18 лет, то в консоли будет "Drive carefully"
+            //Если ему 18, то в консоли "Congratulations, you may apply for your driving license now!"
+        }
+    }
+}
---
+++ InstagramAgeCheck/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InstagramAgeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Приложение спрашивают у юзера возраст, если возраст менее 13 лет, то приложение сообщает 
+            //Что он слишком молод, если более 13 лет, приветствует
+            Console.WriteLine("Hello, please enter your age!");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            if (userAge < 13)
+            {
+                Console.WriteLine("You are too young for Instagram.");
+            } else if (userAge > 13)
+            {
+                Console.WriteLine("Welcome to Instagram!");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You may use Instagram now.");
+            }
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
